cfbfc920d2ad105de4dc10b7656921b0bbc0afd2
zhanghk0527 <44219362+zhanghk0527@users.noreply.github.com>
2018-10-26 10:16:47 +0800

    Update Api_AutoTestSuite.py

--- User_ApiAutoTest/ApiSuite/Api_AutoTestSuite.py
+++ User_ApiAutoTest/ApiSuite/Api_AutoTestSuite.py
@@ -1,7 +1,14 @@
 #-*-coding:utf-8-*-
 import unittest
+import sys
+import os
 from ApiCase import ApiAutoTestCase
 from ApiLog.Api_AutoTestLog import createReport
+#当前目录上层目录的所有文件
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 class ApiAutoTest(unittest.TestSuite):
     AutoSuite = unittest.TestSuite()
     AutoSuite.addTests(unittest.makeSuite(ApiAutoTestCase.ApiAutoTestCase))
