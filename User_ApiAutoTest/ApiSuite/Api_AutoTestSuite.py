0ac59a4348cc91c768361c7aeb45136d21b4fece
zhanghk0527 <44219362+zhanghk0527@users.noreply.github.com>
2018-12-05 16:53:41 +0800

    Update Api_AutoTestSuite.py

--- User_ApiAutoTest/ApiSuite/Api_AutoTestSuite.py
+++ User_ApiAutoTest/ApiSuite/Api_AutoTestSuite.py
@@ -4,9 +4,15 @@ import sys
 import os
 from ApiCase import ApiAutoTestCase
 from ApiLog.Api_AutoTestLog import createReport
-#当前目录上层目录的所有文件
+
+# 返回当前文件所在的绝对路径
+# __file__：获取当前文件的所在路径
 curPath = os.path.abspath(os.path.dirname(__file__))
+
+# 把文件路径分为：文件所在区域（dirname）和当前文件名(basename)，返回一个元组，取第一个元素（以0开始为第一个元素）
 rootPath = os.path.split(curPath)[0]
+
+# 把rootPath添加到环境变量，返回一个列表，执行时从该列表中搜索所需文件，如调用的ApiCase文件夹中的ApiAutoTestCase
 sys.path.append(rootPath)
 
 class ApiAutoTest(unittest.TestSuite):
